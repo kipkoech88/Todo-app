92edfab4b43e1e4319a9f5f162c6767b3bdb7c22
kipkoech88 <96489816+kipkoech88@users.noreply.github.com>
2022-10-09 10:00:19 +0300

    Add files via upload

---
+++ todo/base/models.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from tkinter import Widget
+from django.db import models 
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Tasks(models.Model):
+    user= models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title= models.CharField(max_length=128, null=True, blank=True) 
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False) 
+    create=models.DateField(auto_now_add=True) 
+    def __str__(self):
+        return self.title 
+
+    class Meta:
+        ordering= ["complete"]
